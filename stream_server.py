70061ab0c44bdd3614d9b6a8026c6bc21fa15ba1
Nils_Art <nils.hollingshausen@reija.eu>
2026-03-11 05:21:44 +0100

    -prioritizing m3u8 format

--- stream_server.py
+++ stream_server.py
@@ -33,10 +33,15 @@ def needs_resolution(url: str) -> bool:
 
 def _resolve_sync(url: str) -> dict:
     opts = {
-        'format': 'best[ext=mp4][protocol=https]/best[protocol=m3u8]/best',
+        'format': 'best[protocol=m3u8]/best[ext=mp4][vcodec^=avc1]/best',
         'quiet': True,
         'no_warnings': True,
         'socket_timeout': 15,
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['web'],
+            }
+        },
     }
     with yt_dlp.YoutubeDL(opts) as ydl:
         info = ydl.extract_info(url, download=False)
